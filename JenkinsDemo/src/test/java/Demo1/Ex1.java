4f8665c1ce92f7a41232918734b3eed4faeff954
SayliGit1 <sayliharne4@gmail.com>
2025-03-13 16:27:13 +0530

    jenkin

--- JenkinsDemo/src/test/java/Demo1/Ex1.java
+++ JenkinsDemo/src/test/java/Demo1/Ex1.java
@@ -3,8 +3,9 @@ package Demo1;
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello jenkins");
+
+//after modification ,in jenkin build will start automatically......see this msg their
+		System.out.println("Hello jenkins");
 	}
 
 }
